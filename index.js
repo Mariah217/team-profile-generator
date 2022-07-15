feffd6443f9a369d7d8e4f0a809b273f594178a0
Mariah <mariahjohnston217@outlook.com>
2022-07-14 20:41:07 -0400

    added template literal html outline to index.js file, html file generates

--- index.js
+++ index.js
@@ -3,12 +3,109 @@ import fs from 'fs';
 
 const generateHTML=(
     {
-        managerName, managerID, managerEmail, managerNumber, add, engineerName, engineerID, engineerEmail, engineerGithub, internName, internID, internEmail, internSchool, 
+        managerName, managerID, managerEmail, managerNumber, engineerName, engineerID, engineerEmail, engineerGithub, internName, internID, internEmail, internSchool, 
     }
 ) =>
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href="./style.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+      <title>Team</title>
+    </head>
     
+    <body>
+        <header>My Team</header>
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="card text-black bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-header text-white" style="background-color: rgb(113,52,113)
+                    ">${managerName}
+                    <div id="manager"><i class="fa-solid fa-mug-hot"></i> Manager</div>
+                </div>
+                    
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID:${managerID}</li>
+                            <li class="list-group-item">E-mail:${managerEmail}</li>
+                            <li class="list-group-item">Office Number:${managerNumber}</li>
+                          </ul>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="card text-black bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-header text-white" style="background-color: rgb(113,52,113)
+                    ">${engineerName}
+                    <div id="engineer"><i class="fa-solid fa-glasses"></i> Engineer</div>
+                </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID:${engineerID}  </li>
+                            <li class="list-group-item">E-mail:${engineerEmail}</li>
+                            <li class="list-group-item">GitHub:${engineerGithub} </li>
+                          </ul>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="card text-black bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-header text-white" style="background-color: rgb(113,52,113)
+                    ">${engineerName}
+                 <div id="engineer"><i class="fa-solid fa-glasses"></i> Engineer</div>    
+                </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID:${engineerID} </li>
+                            <li class="list-group-item">E-mail:${engineerEmail}</li>
+                            <li class="list-group-item">GitHub:${engineerGithub}</li>
+                          </ul>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row2" id="row2">
+            <div class="col-sm-3 col-md-3 col-lg-3">
+                <div class="card text-black bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-header text-white" style="background-color: rgb(113,52,113)
+                    ">${engineerName}
+                    <div id="engineer"><i class="fa-solid fa-glasses"></i> Engineer</div> 
+                </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID: ${engineerID} </li>
+                            <li class="list-group-item">E-mail: ${engineerEmail}</li>
+                            <li class="list-group-item">GitHub: ${engineerGithub} </li>
+                          </ul>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-sm-3 col-md-3 col-lg-3">
+                <div class="card text-black bg-light mb-3" style="max-width: 18rem;">
+                    <div class="card-header text-white" style="background-color: rgb(113,52,113)
+                    ">${internName}
+                <div id="intern"><i class="fa-solid fa-user-graduate"></i> Intern</div>    
+                </div>
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID: ${internID}</li>
+                            <li class="list-group-item">E-mail:${internEmail}</li>
+                            <li class="list-group-item">School:${internSchool} </li>
+                          </ul>
+                    </div>
+                  </div>
+              </div>
+        </div>
+    </body>
+    `
 
-const input = [
+const questions = [
     {
         type: 'input',
         message: "What is the team manager's name?",
@@ -21,7 +118,7 @@ const input = [
     },
     {
         type: 'input',
-        message: "What is the team manager's e-mail address",
+        message: "What is the team manager's e-mail address?",
         name: 'managerEmail',
     },
     {
@@ -29,12 +126,12 @@ const input = [
         message: "What is the team manager's office number?",
         name: 'managerNumber',
     },
-    {
-        type: 'list',
-        message: 'Would you like to add an engineer or an intern?',
-        choices: ['Engineer', 'Intern'],
-        name: 'add',
-    },
+    // {
+    //     type: 'list',
+    //     message: 'Would you like to add an engineer or an intern?',
+    //     choices: ['Engineer', 'Intern'],
+    //     name: 'add',
+    // },
     {
         type: 'input',
         message: "What is the engineer's name?",
@@ -81,7 +178,7 @@ const input = [
 
 //function to write index.html file
 inquirer
-.prompt(input)
+.prompt(questions)
 .then(response => {
     console.log(response)
     fs.writeFileSync("index.html", generateHTML(response))
