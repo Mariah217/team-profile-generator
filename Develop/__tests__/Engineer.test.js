a93300685ae1fc8570eb0bfde1fd37f648169bb1
Mariah <mariahjohnston217@outlook.com>
2022-07-16 21:23:13 -0400

    completed intern.js file, started intern.test.js file

--- Develop/__tests__/Engineer.test.js
+++ Develop/__tests__/Engineer.test.js
@@ -1,10 +1,21 @@
 const Employee = require ("../lib/Employee")
+const Engineer = require ("../lib/Engineer")
 
-describe ("Employee", ()=>{
+describe ("Engineer", ()=>{
     it("Test engineer's properties", () =>{
-        const engineer = new Employee ('Brandon' 2, 'brandon@outlook.com')
+        const engineer = new Engineer ('Brandon', 2, 'brandon@outlook.com', 'brandon123')
         expect(engineer.name).toEqual('Brandon')
         expect(engineer.id).toEqual(2)
         expect(engineer.email).toEqual('brandon@outlook.com')
+        expect(engineer.Github).toEqual('brandon123')
+    })
+})
+    describe("Engineer", ()=>{
+        it("Test engineer's methods", ()=>{
+            const engineer = new Employee ('Samantha', 3, 'samantha123@gmail.com', 'samantha30')
+            expect(engineer.getName()).toEqual('Samantha')
+            expect(engineer.getID()).toEqual(3)
+            expect(engineer.getEmail()).toEqual('samantha123@gmail.com')
+            expect(engineer.getGithub()).toEqual('samantha30') //this is not working, not showing up as a method?????
     })
 })
